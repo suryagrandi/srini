c6f3ddaf5fa1ccadc9d8282d35d5a944a33e3d64
suryagrandi <50412397+suryagrandi@users.noreply.github.com>
2019-07-04 14:22:11 +0530

    ioinic changes

---
+++ ex1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Class4
+    {
+        void display()
+        {
+            // calling fuction show() 
+            this.show();
+
+            Console.WriteLine("Inside display function");
+        }
+
+        void show()
+        {
+            Console.WriteLine("Inside show funcion");
+        }
+
+        // Main Method 
+        public static void Main(String[] args)
+        {
+            Class4 t1 = new Class4();
+            t1.display();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ex2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Class2
+    {
+        static void Main(string[] args)
+        {
+            string text = "HYD";
+            char[] cArray = text.ToCharArray();
+
+            for (int i = cArray.Length - 1; i > -1; i--)
+            {
+
+                if (cArray[i] % 2 == 0)
+                {
+                    char reverse = cArray[i];
+                    Console.Write(reverse);
+                }
+                else
+                {
+                    char reverse1 = cArray[i];
+                    Console.Write("_" + reverse1 + "_");
+
+                }
+            }
+            Console.ReadLine();
+
+        }
+
+    }
+}
